f3c14ca813c36d7b18d9767f678643cda8b74f2f
memstudios <michaelmurphystudios@gmail.com>
2017-11-11 11:36:56 -0500

    added other files to be tracked

--- functions.php
+++ functions.php
@@ -71,3 +71,10 @@ require get_template_directory() . '/inc/woocommerce.php';
  * Load Editor functions.
  */
 require get_template_directory() . '/inc/editor.php';
+
+/*
+ * Change file upload size
+*/
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
